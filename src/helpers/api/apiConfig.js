8e13dc0c54dca78d2606da347264ada6d4675f37
Ivan <bojenka229@gmail.com>
2021-10-24 21:54:59 +0300

    + add dist

--- src/helpers/api/apiConfig.js
+++ src/helpers/api/apiConfig.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import apiKey from "./apiKey";
+import apiKey from "../apiKey";
 
 export function fetchWeatherForecast() {
   return Axios.get(
---
+++ src/helpers/apiKey/index.js
@@ -0,0 +1,5 @@
+const apiKey = "489a96d491msh54c10c1aa291386p1fc25djsnf5a3a81bae62"
+
+export default {
+    apiKey
+}
